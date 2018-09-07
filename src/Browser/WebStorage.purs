645c4e96149ca7e674d18ce54689c72b290b875d
colin-passiv <9568290+colin-passiv@users.noreply.github.com>
2018-09-07 08:29:27 +0100

    migrate to PureScript 0.12

--- src/Browser/WebStorage.purs
+++ src/Browser/WebStorage.purs
@@ -3,8 +3,6 @@ module Browser.WebStorage
   , LocalStorage(..)
   , StorageKey(..)
   , SessionStorage(..)
-  , WEB_STORAGE()
-  , EffWebStorage()
   , localStorage
   , sessionStorage
   , clear
@@ -15,30 +13,27 @@ module Browser.WebStorage
   , setItem
   ) where
 
-  
+
 import Prelude
 
-import Control.Monad.Eff (Eff, kind Effect)
+import Effect (Effect)
 import Data.Function.Uncurried (Fn3, Fn2, runFn3, runFn2)
 import Data.Maybe (Maybe(..))
-
-foreign import data WEB_STORAGE :: Effect
-type EffWebStorage eff = Eff (webStorage :: WEB_STORAGE | eff)
 
 newtype StorageKey = StorageKey String
 
 class Storage s where
-    clear :: forall eff. s -> EffWebStorage eff Unit
-    getItem :: forall eff. s -> StorageKey -> EffWebStorage eff (Maybe String)
-    key :: forall eff. s -> Int -> EffWebStorage eff (Maybe StorageKey)
-    length :: forall eff. s -> EffWebStorage eff Int
-    removeItem :: forall eff. s -> StorageKey -> EffWebStorage eff Unit
-    setItem :: forall eff. s -> StorageKey -> String -> EffWebStorage eff Unit
+    clear :: s -> Effect Unit
+    getItem :: s -> StorageKey -> Effect (Maybe String)
+    key :: s -> Int -> Effect (Maybe StorageKey)
+    length :: s -> Effect Int
+    removeItem :: s -> StorageKey -> Effect Unit
+    setItem :: s -> StorageKey -> String -> Effect Unit
 
 instance storageLocalStorage :: Storage LocalStorage where
     length _ = unsafeLength localStorage
     key _ n = runFn3 unsafeKey (null2Maybe StorageKey) localStorage n
-    getItem _ k = runFn3 unsafeGetItem (null2Maybe id) localStorage k
+    getItem _ k = runFn3 unsafeGetItem (null2Maybe identity) localStorage k
     setItem _ k v = runFn3 unsafeSetItem localStorage k v
     removeItem _ k = runFn2 unsafeRemoveItem localStorage k
     clear _ = unsafeClear localStorage
@@ -46,7 +41,7 @@ instance storageLocalStorage :: Storage LocalStorage where
 instance storageSessionStorage :: Storage SessionStorage where
     length _ = unsafeLength sessionStorage
     key _ n = runFn3 unsafeKey (null2Maybe StorageKey) sessionStorage n
-    getItem _ k = runFn3 unsafeGetItem (null2Maybe id) sessionStorage k
+    getItem _ k = runFn3 unsafeGetItem (null2Maybe identity) sessionStorage k
     setItem _ k v = runFn3 unsafeSetItem sessionStorage k v
     removeItem _ k = runFn2 unsafeRemoveItem sessionStorage k
     clear _ = unsafeClear sessionStorage
@@ -58,22 +53,22 @@ foreign import localStorage :: LocalStorage
 foreign import sessionStorage :: SessionStorage
 
 foreign import unsafeLength
-    :: forall eff storage. storage -> EffWebStorage eff Int
+    :: forall storage. storage -> Effect Int
 
 foreign import unsafeKey
-    :: forall eff storage. Fn3 (String -> Maybe StorageKey) storage Int (EffWebStorage eff (Maybe StorageKey))
+    :: forall storage. Fn3 (String -> Maybe StorageKey) storage Int (Effect (Maybe StorageKey))
 
 foreign import unsafeGetItem
-    :: forall eff storage. Fn3 (String -> Maybe String) storage StorageKey (EffWebStorage eff (Maybe String))
+    :: forall storage. Fn3 (String -> Maybe String) storage StorageKey (Effect (Maybe String))
 
 foreign import unsafeSetItem
-    :: forall eff storage. Fn3 storage StorageKey String (EffWebStorage eff Unit)
+    :: forall storage. Fn3 storage StorageKey String (Effect Unit)
 
 foreign import unsafeRemoveItem
-    :: forall eff storage. Fn2 storage StorageKey (EffWebStorage eff Unit)
+    :: forall storage. Fn2 storage StorageKey (Effect Unit)
 
 foreign import unsafeClear
-    :: forall eff storage. storage -> EffWebStorage eff Unit
+    :: forall storage. storage -> Effect Unit
 
 foreign import null2MaybeImpl
     :: forall a b. Fn3 (a -> Maybe b) (Maybe b) a (Maybe b)
